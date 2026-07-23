69fefcb24093a11283f0c842a543c0d2b2f6ac38
jf2024 <josexfuentes27@gmail.com>
2026-07-22 21:27:49 -0700

    numpy done

--- assignments_01/warmup_01.py
+++ assignments_01/warmup_01.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # --- Part 1: Pandas ---
 
@@ -45,3 +46,52 @@ df = pd.DataFrame(data)
 #print(df.sort_values("grade", ascending=False))
 
 # --- Part 1: Numpy ---
+
+# Numpy Q1
+# arr1 = np.array([10, 20, 30, 40, 50])
+# print(f"The shape is: {arr1.shape}")
+# print("------------------------")
+# print(f"Data Type is: {arr1.dtype}")
+# print("------------------------")
+# print(f"Dimension is:\n {arr1.ndim}")
+# print("===========================================")
+
+
+# Numpy Q2
+arr2 = np.array([[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]])
+
+# print(f"The shape is: {arr2.shape}")
+# print("------------------------")
+# print(f"Size is: {arr2.size}")
+# print("===========================================")
+
+
+# Numpy Q3
+# print(arr2[0:2, 0:2])
+# print("===========================================")
+
+# Numpy Q4
+# arr_zeros = np.zeros((3, 4))
+# print(arr_zeros)
+# print("------------------------")
+# arr_ones = np.ones((2, 5))
+# print(arr_ones)
+# print("===========================================")
+
+# Numpy Q5
+# arr_step = np.arange(0, 50, 5)
+# print(arr_step)
+# print('Shape:', arr_step.shape)
+# print("Mean:", np.mean(arr_step)) 
+# print("Sum:", np.sum(arr_step)) 
+# print("Standard Deviation:", np.std(arr_step)) 
+# print("===========================================")
+
+# Numpy Q6
+# arr_stan = np.random.normal(size=200)
+# print("Mean:", np.mean(arr_stan))
+# print("Standard Deviation:", np.std(arr_stan))
+
+# --- Part 1: Matplotlib ---
