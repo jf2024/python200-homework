8c658fc7eefe156e45fc1b8f109d987f82c2c9f7
jf2024 <josexfuentes27@gmail.com>
2026-08-07 21:05:27 -0700

    putting data in directory

--- assignments_01/project_01.py
+++ assignments_01/project_01.py
@@ -1,1 +1,9 @@
-# placeholder
+from prefect import task, flow
+from prefect.logging import get_run_logger 
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import ttest_ind
+
+@task 
+def load_data():
+    logger = get_run_logger()
